efa85dea189db33dd7e7491d47188b50d825fab7
Henning Schwentner <hs@wps.de>
2021-07-30 15:10:51 +0200

    Allow opening quotation marks in body text

--- lib/Markua2Styles.pm
+++ lib/Markua2Styles.pm
@@ -123,10 +123,10 @@ sub translateBodyText {
     $text =~ s/^I> (.*)$/::: {custom-style="$styles{'SF1_TTL'}"}\nNote\n:::\n::: {custom-style="$styles{'SF1_FIRST'}"}\n$1\n:::/gm;
     $text =~ s/^T> (.*)$/::: {custom-style="$styles{'SF2_TTL'}"}\nTip\n:::\n::: {custom-style="$styles{'SF2_FIRST'}"}\n$1\n:::/gm;
     # Paragraphs
-    $text =~ s/((?:^|\n)#.*(?:\n+>.*)?(?:\n+\!.*)?)\n\n+([A-Za-z\[\*].*)\n+/$1\n\n::: {custom-style="$styles{'HEADFIRST'}"}\n$2\n:::\n\n/gm; # First paragraph after heading with optional epigraph and optional opening picture
-    $text =~ s/((?:^|\n)#.*(?:\n+\!.*)?)\n\n+([A-Za-z\[\*].*)\n+/$1\n\n::: {custom-style="$styles{'HEADFIRST'}"}\n$2\n:::\n\n/gm; # Doppelt für gerade Absatznummer
-    $text =~ s/\n\n([A-Za-z\[\*].*)\n+/\n\n::: {custom-style="$styles{'CHAP_BM'}"}\n$1\n:::\n\n/gm;  # einmal für ungerade Absatznummer
-    $text =~ s/\n\n([A-Za-z\[\*].*)\n+/\n\n::: {custom-style="$styles{'CHAP_BM'}"}\n$1\n:::\n\n/gm;  # Doppelt für Gerade Absatznummer
+    $text =~ s/((?:^|\n)#.*(?:\n+>.*)?(?:\n+\!.*)?)\n\n+([A-Za-z“„»\[\*].*)\n+/$1\n\n::: {custom-style="$styles{'HEADFIRST'}"}\n$2\n:::\n\n/gm; # First paragraph after heading with optional epigraph and optional opening picture
+    $text =~ s/((?:^|\n)#.*(?:\n+\!.*)?)\n\n+([A-Za-z“„»\[\*].*)\n+/$1\n\n::: {custom-style="$styles{'HEADFIRST'}"}\n$2\n:::\n\n/gm; # Doppelt für gerade Absatznummer
+    $text =~ s/\n\n([A-Za-z“„»\[\*].*)\n+/\n\n::: {custom-style="$styles{'CHAP_BM'}"}\n$1\n:::\n\n/gm;  # einmal für ungerade Absatznummer
+    $text =~ s/\n\n([A-Za-z“„»\[\*].*)\n+/\n\n::: {custom-style="$styles{'CHAP_BM'}"}\n$1\n:::\n\n/gm;  # Doppelt für Gerade Absatznummer
     # Epigraphs/Block Quotations
     $text =~ s/(^# .*\n+)> (.*)(—.*)$/$1::: {custom-style="$styles{'CF_EPG_FIRST'}"}\n$2\n:::\n::: {custom-style="$styles{'CF_EPG_ATTR_AU_NA'}"}\n$3\n:::\n/gm; # Opening epigraph
     $text =~ s/(^##+ .*\n+)> (.*)(—.*)$/$1::: {custom-style="$styles{'EPG'}"}\n$2\n:::\n::: {custom-style="$styles{'EPG_ATTR_AU_NA'}"}\n$3\n:::/gm;  # Epigraph with author
